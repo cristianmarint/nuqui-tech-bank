8d5ea6bc7254041dec6f6275475f3f66a55cefa2
Cristian Marin <31068007+cristianmarint@users.noreply.github.com>
2024-01-01 15:48:18 -0500

    queries

--- infrastructure/dynamodb.sh
+++ infrastructure/dynamodb.sh
@@ -14,3 +14,8 @@ aws dynamodb create-table `
         'IndexName=usernameindex,KeySchema=[{AttributeName=username,KeyType=HASH}],Projection={ProjectionType=ALL},ProvisionedThroughput={ReadCapacityUnits=5,WriteCapacityUnits=5}' `
     --endpoint-url http://localhost:8000 `
     --profile default
+
+aws dynamodb put-item --table-name users --item '{\"deletedAt\": {\"S\": \"2024-01-01T20:34:22.492619700Z\"},\"email\": {\"S\": \"celestine96@hotmail.com\"}, \"humanId\": {\"N\": \"7\"}, \"id\": {\"S\": \"12ebc8ee-ff66-4455-ad3b-6a7c7413ac90\"}, \"password\": {\"S\": \"password\"}, \"recentActivity\": {\"S\": \"LOGIN SUCCESSFUL AT 2024-01-01T20:34:25.627823900Z\"}, \"status\": {\"S\": \"ACTIVE\"}, \"token\": {\"S\": \"eyJhbGciOiJub25lIn0.eyJzdWIiOiJ1c2VybmFtZSIsImlhdCI6MTcwNDE0MTI2NSwiZXhwIjoxNzA0MTQxMjk1fQ.\"}, \"username\": {\"S\": \"username\"}}' --endpoint-url http://localhost:8000 --profile default
+
+
+
--- infrastructure/queries.sql
+++ infrastructure/queries.sql
@@ -1,1 +1,4 @@
 select * from humans;
+
+INSERT INTO humans (id, name, lastname, identification, email, username, password)
+VALUES (7, 'Eliezer', 'Thompson', 750, 'celestine96@hotmail.com', 'username', 'password');
